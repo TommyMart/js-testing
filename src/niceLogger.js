f02897fe595dc24dcc361e67c418c4a790255fc7
Thomas Martin <161907136+TommyMart@users.noreply.github.com>
2024-08-24 10:24:29 +0930

    feat: test coverage command

--- src/niceLogger.js
+++ src/niceLogger.js
@@ -1,8 +1,24 @@
-function niceLogger() {
-    console.log("Hello world!");
-    return "Hello world!";
+function niceLogger(){
+	console.log("Hello world!");
+	return "Hello world!";
+}
+
+function messageRepeater(messageToRepeat){
+
+	if (Array.isArray(messageToRepeat)){
+
+		return [...messageToRepeat, ...messageToRepeat, ...messageToRepeat];
+
+	} else {
+		let localMessageToRepeat = messageToRepeat.toString();
+
+		return localMessageToRepeat + localMessageToRepeat + localMessageToRepeat;
+	}
+
+	
 }
 
 module.exports = {
-    niceLogger
+	niceLogger,
+	messageRepeater
 }
--- test/niceLogger.test.js
+++ test/niceLogger.test.js
@@ -1,7 +1,65 @@
-const {niceLogger} = require("../src/niceLogger.js")
+// Import a specific function from the file 
+const {niceLogger, messageRepeater} = require("../src/niceLogger.js");
+
+// import * as SomeFile from "../src/niceLogger.js";
 
 
-test("niceLogger return Hello world", () => {
+// Import the file 
+const niceLoggerFile = require("../src/niceLogger.js");
+// Import a specific function from the file 
+const niceLoggerFunction = niceLoggerFile.niceLogger;
 
-    expect(niceLogger()).toBe("Hello world!");
+
+describe("niceLogger function tests", () => {
+	test("niceLogger returns Hello world!", () => {
+
+		expect(niceLogger()).toBe("Hello world!");
+	});	
+
+	test("niceLogger returns Hello world!", () => {
+
+		expect(niceLogger()).toBe("Hello world!");
+	});	
+
+	test("niceLogger returns Hello world!", () => {
+
+		expect(niceLogger()).toBe("Hello world!");
+	});	
+
+	test("niceLogger returns Hello world!", () => {
+
+		expect(niceLogger()).toBe("Hello world!");
+	});	
+});
+
+
+describe("messageRepeater function tests", () => {
+
+	test("messageRepeater repeats words correctly", () => {
+		let repeatedMessage = messageRepeater("hello");
+
+		expect(repeatedMessage).toEqual("hellohellohello");
+		expect(repeatedMessage).toHaveLength(15);
+	});
+
+	test("messageRepeater repeats numbers correctly", () => {
+		let repeatedMessage = messageRepeater(1);
+
+		expect(repeatedMessage).toEqual("111");
+		expect(repeatedMessage).toHaveLength(3);
+	});
+
+	test("messageRepeater repeats arrays correctly", () => {
+		let repeatedMessage = messageRepeater(["hello", "world"]);
+		console.log(repeatedMessage);
+		expect(repeatedMessage).toEqual(["hello", "world", "hello", "world", "hello", "world"]);
+		expect(repeatedMessage).toHaveLength(6);
+	});
+
+	test("messageRepeater repeats null or whitespace correctly", () => {
+		let repeatedMessage = messageRepeater("");
+
+		expect(repeatedMessage).toEqual("");
+		expect(repeatedMessage).toHaveLength(0);
+	});
 });
